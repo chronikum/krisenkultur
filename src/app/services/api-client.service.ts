4ac9b21c7c96a98fc7e3b7e58100e8cf40f5f597
rupprecr <rupprecr@tf.uni-freiburg.de>
2020-03-17 01:46:37 +0100

    added models for tag and event, updated api client

--- src/app/services/api-client.service.ts
+++ src/app/services/api-client.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { filter, map, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import {Event} from '../models/event';
+import {Tag} from '../models/tag';
 
 /**
  * Service to communicate with the backend
@@ -23,6 +25,13 @@ export class ApiClientService {
   }
 
   /**
+   * Get all events
+   */
+  getAll(): Observable<any[]> {
+    return this.http.get<any[]>(this.endpoint + '?get_all')
+  }
+
+  /**
    * Get all tags from server
    */
   getAllTags(): Observable<any[]> {
@@ -37,8 +46,21 @@ export class ApiClientService {
   /**
    * Get tags for event
    */
-  tagsForEvent(id: number): Observable<any[]> {
+  getTagsForEvent(id: number): Observable<Tag[]> {
     return this.http.get<any[]>(this.endpoint + '?get_taggs_for_event=' + id)
   }
 
+  /**
+   * Get event by id
+   */
+  getEventsById(id: number): Observable<Event> {
+    return this.http.get<Event>(this.endpoint + '?get_by_id=' + id)
+  }
+
+  /**
+   * Get events by date (%d.%m.%Y)
+   */
+  getEventsByDate(date: string): Observable<Event[]> {
+    return this.http.get<Event[]>(this.endpoint + '?get_events_by_date=' + date)
+  }
 }
